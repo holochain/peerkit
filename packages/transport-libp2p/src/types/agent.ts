e17df9f9e1fee629f035f70c75c393ee2e25a77f
Jost Schulte <jost-s@users.noreply.github.com>
2026-04-29 11:04:42 -0600

    fixup! feat: write listening address identification

--- packages/transport-libp2p/src/types/agent.ts
+++ packages/transport-libp2p/src/types/agent.ts
@@ -1,17 +1,7 @@
-import type { Multiaddr } from "@multiformats/multiaddr";
-
 /**
  * Identifier for an agent
  */
 export type AgentId = Uint8Array;
-
-/**
- * Address to dial a peer
- */
-export interface IAgentInfo {
-  id: AgentId;
-  addr: Multiaddr;
-}
 
 export interface RelayConfig {
   canRelay: boolean;
