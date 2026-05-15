04514e9003f92ed7b3482dd2b3628945f9eb8d88
Jost <jost-s@users.noreply.github.com>
2026-05-14 18:13:26 -0600

    refactor: provide builders for relay + node

--- packages/peerkit/tests/transport-libp2p/relay.test.ts
+++ packages/peerkit/tests/transport-libp2p/relay.test.ts
@@ -1,12 +1,12 @@
-import { afterEach, beforeEach, expect, test, vi } from "vitest";
-import { PeerkitNode } from "../../src/node.js";
-import { PeerkitRelay } from "../../src/relay.js";
-import getPort, { portNumbers } from "get-port";
-import type { RelayAddress } from "@peerkit/api";
-import { setupTestLogger } from "./util.js";
 import { reset } from "@logtape/logtape";
-import { AgentKeyPair } from "../../src/agent.js";
+import type { RelayAddress } from "@peerkit/api";
+import getPort, { portNumbers } from "get-port";
+import { afterEach, beforeEach, expect, test, vi } from "vitest";
 import { buildOwnAgentInfo } from "../../src/agent-info.js";
+import { AgentKeyPair } from "../../src/agent.js";
+import { PeerkitNodeBuilder } from "../../src/node.js";
+import { PeerkitRelayBuilder } from "../../src/relay.js";
+import { setupTestLogger } from "./util.js";
 
 beforeEach(setupTestLogger);
 
@@ -16,19 +16,19 @@ test("Relay sends known agents to connecting peer", async () => {
   // Create the relay
   const relayPort = await getPort({ port: portNumbers(30_000, 40_000) });
   const relayAddress: RelayAddress = `/ip4/0.0.0.0/tcp/${relayPort}`;
-  const relay = await PeerkitRelay.create({
-    id: "relay",
-    addrs: [relayAddress],
-    networkAccessHandler: async () => true,
-  });
+  const relay = await new PeerkitRelayBuilder(async () => true)
+    .withId("relay")
+    .withAddresses([relayAddress])
+    .build();
 
   // Create node 1 with the relay address. Access handler allows everyone.
-  const node1 = await PeerkitNode.create({
-    id: "node1",
+  const node1 = await new PeerkitNodeBuilder({
     networkAccessHandler: async () => true,
     messageHandler: async () => {},
-    bootstrapRelays: [relayAddress],
-  });
+  })
+    .withId("node1")
+    .withBootstrapRelays([relayAddress])
+    .build();
 
   // Wait for node 1 to connect to relay and add its own address to
   // its agent store.
@@ -46,12 +46,13 @@ test("Relay sends known agents to connecting peer", async () => {
   );
 
   // Create node 2
-  const node2 = await PeerkitNode.create({
-    id: "node2",
+  const node2 = await new PeerkitNodeBuilder({
     networkAccessHandler: async () => true,
     messageHandler: async () => {},
-    bootstrapRelays: [relayAddress],
-  });
+  })
+    .withId("node2")
+    .withBootstrapRelays([relayAddress])
+    .build();
 
   // Wait for node 2 to add its own agent info to its agent store.
   const node2AgentInfoSigned = await vi.waitUntil(
@@ -82,20 +83,20 @@ test("Node connects to another node using address learned from relay", async () 
   // Create the relay
   const relayPort = await getPort({ port: portNumbers(30_000, 40_000) });
   const relayAddress: RelayAddress = `/ip4/0.0.0.0/tcp/${relayPort}`;
-  const relay = await PeerkitRelay.create({
-    id: "relay",
-    addrs: [relayAddress],
-    networkAccessHandler: async () => true,
-  });
+  const relay = await new PeerkitRelayBuilder(async () => true)
+    .withId("relay")
+    .withAddresses([relayAddress])
+    .build();
 
   // Create node 1 and wait until it has registered its own agent info via the
   // relay.
-  const node1 = await PeerkitNode.create({
-    id: "node1",
+  const node1 = await new PeerkitNodeBuilder({
     networkAccessHandler: async () => true,
     messageHandler: async () => {},
-    bootstrapRelays: [relayAddress],
-  });
+  })
+    .withId("node1")
+    .withBootstrapRelays([relayAddress])
+    .build();
   await vi.waitUntil(() => node1.agentStore.get(node1.keyPair.agentId()), {
     timeout: 5_000,
   });
@@ -112,12 +113,13 @@ test("Node connects to another node using address learned from relay", async () 
   node1.agentStore.store([thirdAgentInfo]);
 
   // Create node 2
-  const node2 = await PeerkitNode.create({
-    id: "node2",
+  const node2 = await new PeerkitNodeBuilder({
     networkAccessHandler: async () => true,
     messageHandler: async () => {},
-    bootstrapRelays: [relayAddress],
-  });
+  })
+    .withId("node2")
+    .withBootstrapRelays([relayAddress])
+    .build();
   // Wait for node 2 to register its own agent info, to send to node 1 later.
   const node2AgentInfo = await vi.waitUntil(
     () => node2.agentStore.get(node2.keyPair.agentId()),
